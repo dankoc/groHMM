f523b483f3316a72280db8ff6134117b21456d9c
mchae <minho.chae@gmail.com>
2013-03-06 16:31:25 -0600

    readBed;3 columns

--- R/readBed.R
+++ R/readBed.R
@@ -1,7 +1,7 @@
 #' readBed Returns a GenomicRanges object constrcuted from the specified bed file. 
 #'
 #' Bed file format is assumed to be either four column: seqnames, start, end, strand columns; or six column: seqnames, start, end, name, score, and strand.  
-#' There is currently no support for bed files without strand information.  
+#' Three column format is also possible when there is no strand information. 
 #'
 #' Any additional arguments availiable to read.table can be specified.  
 #'
@@ -10,10 +10,13 @@
 #' @author Minho Chae and Charles G. Danko.
 readBed <- function(file, ...) {
     df <- read.table(file, ...)
-	## Do we want an option for a 3-column bed (i.e. strand not specified)?  It's technically a legal UCSC format.  
+	if(NCOL(df) == 3) {
+		col.names(df) <- c("seqnames", "start", "end")
+		df <- cbind(df, strand=Rle("*", NROW(df)))
+	}
 	if(NCOL(df) == 4) col.names(df) <- c("seqnames", "start", "end", "strand")
-    if(NCOL(df) == 6) col.names(df) <- c("seqnames", "start", "end", "name", "score", "strand")
-    return( GRanges(seqnames = Rle(df$seqnames), ranges = IRanges(df$start, df$end),
+	if(NCOL(df) == 6) col.names(df) <- c("seqnames", "start", "end", "name", "score", "strand")
+	return( GRanges(seqnames = Rle(df$seqnames), ranges = IRanges(df$start, df$end),
             strand = Rle(strand(df$strand))))
 }
 
