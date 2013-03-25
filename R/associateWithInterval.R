bb1fea1b3a987ba9bdb9298775f4acbab3cb2b5d
Charles Danko <dankoc@gmail.com>
2013-03-25 09:49:41 -0400

    Testing parallel on associateWithInterval

--- R/associateWithInterval.R
+++ R/associateWithInterval.R
@@ -37,42 +37,51 @@
 ##
 ########################################################################
 
+
+associateWithInterval_foreachChrom <- function(i) {
+	# Which KG?  prb?
+	indxF   <- which(f[[1]] == C[i])
+	indxPrb <- which(p[[1]] == C[i])
+
+	if((NROW(indxF) >0) & (NROW(indxPrb) >0)) {
+		# Type coersions.
+		FeatureStart <- as.integer(f[[2]][indxF])
+		FeatureEnd <- as.integer(f[[3]][indxF])
+		PROBEStart <- as.integer(p[[2]][indxPrb])
+		PROBELength <- as.integer(p[[3]][indxPrb] - p[[2]][indxPrb])
+
+		# Set dimensions.
+		dim(PROBEStart) <- c(NROW(PROBEStart), NCOL(PROBEStart))
+		dim(FeatureStart) <- c(NROW(FeatureStart), NCOL(FeatureStart))
+		dim(FeatureEnd) <- c(NROW(FeatureEnd), NCOL(FeatureEnd))
+		dim(PROBELength) <- c(NROW(PROBELength), NCOL(PROBELength))
+
+		Fprime <- .Call("AssociateRegionWithFeatures", FeatureStart, FeatureEnd, PROBEStart, PROBELength, PACKAGE = "groHMM")
+	}
+	
+	return(Fprime)
+}
+
 #' associateWithInterval associates reads with the first feature that they fall inside of.
 #'
 #' @param features A GRanges object representing a set of genomic coordinates.  The meta-plot will be centered on the start position.
 #' @param reads A GRanges object representing a set of mapped reads.
 #' @return Returns index of the feature in which a each read is found.  Will be a vector of integers, the same size as the number of reads.  NA indicates that the reads does not fall inside of any feature.
 #' @author Charles G. Danko and Minho Chae
-associateWithInterval <- function(features, reads) {
+associateWithInterval <- function(features, reads, ...) {
         f <- data.frame(chrom=as.character(seqnames(features)), start=as.integer(start(features)),
                                 end=as.integer(end(features)), strand=as.character(strand(features)))
         p <- data.frame(chrom=as.character(seqnames(reads)), start=as.integer(start(reads)),
                                 end=as.integer(end(reads)), strand=as.character(strand(reads))) 
 
 	C <- as.character(unique(p[[1]]))
+	mcp <- mclapply(c(1:NROW(C)), associateWithInterval_foreachChrom, ...)
+
+	## Translate this into a return vector...
 	F <- rep(NA, NROW(p))
 	for(i in 1:NROW(C)) {
-		# Which KG?  prb?
-		indxF   <- which(f[[1]] == C[i])
 		indxPrb <- which(p[[1]] == C[i])
-
-		if((NROW(indxF) >0) & (NROW(indxPrb) >0)) {
-			# Type coersions.
-			FeatureStart <- as.integer(f[[2]][indxF])
-			FeatureEnd <- as.integer(f[[3]][indxF])
-			PROBEStart <- as.integer(p[[2]][indxPrb])
-			PROBELength <- as.integer(p[[3]][indxPrb] - p[[2]][indxPrb])
-
-			# Set dimensions.
-			dim(PROBEStart) <- c(NROW(PROBEStart), NCOL(PROBEStart))
-			dim(FeatureStart) <- c(NROW(FeatureStart), NCOL(FeatureStart))
-			dim(FeatureEnd) <- c(NROW(FeatureEnd), NCOL(FeatureEnd))
-			dim(PROBELength) <- c(NROW(PROBELength), NCOL(PROBELength))
-
-			Fprime <- .Call("AssociateRegionWithFeatures", FeatureStart, FeatureEnd, PROBEStart, PROBELength, PACKAGE = "groHMM")
-
-			F[indxPrb] <- as.character(f[[4]][indxF][as.vector(Fprime)])
-		}
+		F[indxPrb] <- as.character(f[[4]][indxF][as.vector(F[[i]])])
 	}
 
     return(F)
