38cd5159e6787648832e17e19d80b592c6b83310
mchae <minho.chae@gmail.com>
2013-03-28 09:56:02 -0500

    Parallel windowAnalysis

--- R/windowAnalysis.R
+++ R/windowAnalysis.R
@@ -46,7 +46,7 @@
 ##
 ########################################################################
 
-windowAnalysis_foreachChrom <- function(i) {
+windowAnalysis_foreachChrom <- function(i, p, strand, wsize, ssize, chrom, start, limitPCRDups, end, debug=FALSE) {
 	if(debug) {
 		print(chrom[i])
 	}
@@ -113,6 +113,7 @@ windowAnalysis_foreachChrom <- function(i) {
 #' @param end The end position in genomic coordinates.  Default: returns all avaliable data.
 #' @param limitPCRDups Counts only one read mapping to each start site.  NOTE: If set to TRUE, assumes that all reads are the same length (don't use for paired-end data).  Default: FALSE.  
 #' @param debug If set to TRUE, provides additional print options. Default: FALSE
+#' @param ... Extra argument passed to mclapply
 #' @return List object, each element of which represents a chromosome.
 #' @author Charles G. Danko and Minho Chae
 windowAnalysis <- function(reads, strand="N", window_size=(step_size-1), step_size=(window_size+1), chrom=NULL, start=0, end=NULL, limitPCRDups=FALSE, debug=FALSE, ...) { 
@@ -134,7 +135,8 @@ windowAnalysis <- function(reads, strand="N", window_size=(step_size-1), step_si
 		endChrom <- as.integer(end)
 	}
 
-	H <- mclapply(c(1:NROW(chrom)), windowAnalysis_foreachChrom, ...)
+        H <- mclapply(c(1:NROW(chrom)), windowAnalysis_foreachChrom, p=p, strand=strand, wsize=wsize, 
+			ssize=ssize, chrom=chrom, start=start, limitPCRDups=limitPCRDups, end=end, debug=debug, ...)
 	
 	return(H)
 
